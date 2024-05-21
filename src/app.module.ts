370a97f9bb68bc418d8ff938d35178b4cfe43d98
devanlosoft <alopezleon95@correo.unicordoba.edu.co>
2024-05-21 07:01:56 -0500

    modules add to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { IntencionesModule } from './intenciones/intenciones.module';
+import { MessagesModule } from './messages/messages.module';
+import { TextosasociadosIntencionesModule } from './textosasociados_intenciones/textosasociados_intenciones.module';
 
 @Module({
-  imports: [IntencionesModule],
+  imports: [IntencionesModule, MessagesModule, TextosasociadosIntencionesModule],
   controllers: [AppController],
   providers: [AppService],
 })
