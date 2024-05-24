6ff2be16006f167504cc67d94e2bd84e0c430dde
devanlosoft <alopezleon95@correo.unicordoba.edu.co>
2024-05-24 13:37:58 -0500

    messages service

--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -5,22 +5,87 @@ import { UpdateMessageDto } from './dto/update-message.dto';
 @Injectable()
 export class MessagesService {
   create(createMessageDto: CreateMessageDto) {
-    return 'This action adds a new message';
+    return createMessageDto;
   }
 
   findAll() {
-    return `This action returns all messages`;
+    return [
+      {
+        id: "1",
+        emisor: "user1",
+        receptor: "system",
+        verificacion_enviado: true,
+        verificacion_recibido: true,
+        hora_enviado: "11:17",
+        hora_recibido: "11:17",
+        fecha_enviado: "11/08/2001",
+        fecha_recibido: "11/08/2001",
+        id_usuario: "663a86ce6212d88b61542fb5",
+        intenciones: ["6601eb7ca50e70583f4617f0", "6601e901d47cf7ce050839c5"]
+    }
+    ,      {
+      id: "2",
+      emisor: "user2",
+      receptor: "system",
+      verificacion_enviado: true,
+      verificacion_recibido: true,
+      hora_enviado: "11:17",
+      hora_recibido: "11:17",
+      fecha_enviado: "11/08/2001",
+      fecha_recibido: "11/08/2001",
+      id_usuario: "663a86ce6212d88b61542fb5",
+      intenciones: ["6601eb7ca50e70583f4617f0", "6601e901d47cf7ce050839c5"]
+  },
+  {
+    id: "3",
+    emisor: "user3",
+    receptor: "system",
+    verificacion_enviado: true,
+    verificacion_recibido: true,
+    hora_enviado: "11:17",
+    hora_recibido: "11:17",
+    fecha_enviado: "11/08/2001",
+    fecha_recibido: "11/08/2001",
+    id_usuario: "663a86ce6212d88b61542fb5",
+    intenciones: ["6601eb7ca50e70583f4617f0", "6601e901d47cf7ce050839c5"]
+}
+    ];
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} message`;
+   
+    return {
+      id: `${id}`,
+      emisor: "user1",
+      receptor: "system",
+      verificacion_enviado: true,
+      verificacion_recibido: true,
+      hora_enviado: "11:17",
+      hora_recibido: "11:17",
+      fecha_enviado: "11/08/2001",
+      fecha_recibido: "11/08/2001",
+      id_usuario: "663a86ce6212d88b61542fb5",
+      intenciones: ["6601eb7ca50e70583f4617f0", "6601e901d47cf7ce050839c5"]
+    };
   }
 
   update(id: number, updateMessageDto: UpdateMessageDto) {
-    return `This action updates a #${id} message`;
+    return `id ${id} actualizado: ` + updateMessageDto;
   }
 
   remove(id: number) {
-    return `This action removes a #${id} message`;
+  return {
+    id: `${id}`,
+    emisor: "user1",
+    receptor: "system",
+    verificacion_enviado: true,
+    verificacion_recibido: true,
+    hora_enviado: "11:17",
+    hora_recibido: "11:17",
+    fecha_enviado: "11/08/2001",
+    fecha_recibido: "11/08/2001",
+    id_usuario: "663a86ce6212d88b61542fb5",
+    intenciones: ["6601eb7ca50e70583f4617f0", "6601e901d47cf7ce050839c5"]
+    };
   }
 }
